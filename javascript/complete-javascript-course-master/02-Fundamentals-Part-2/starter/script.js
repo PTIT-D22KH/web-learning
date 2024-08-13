f4a2b670fe87b7090614930dd1bcc315b0056cc0
vctduong <165612382+vctduong@users.noreply.github.com>
2024-08-13 12:52:40 +0000

    edit files

--- javascript/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ javascript/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,14 +1,49 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) {
-    hasDriverLicense = true;
-}
-if (hasDriversLicense) {
-    console.log("I can drive :D");
-}
+// if (passTest) {
+//     hasDriverLicense = true;
+// }
+// if (hasDriversLicense) {
+//     console.log("I can drive :D");
+// }
 
-const interface = 'Audio';
-const private = 534;
+// const interface = 'Audio';
+// const private = 534;
+
+
+// function logger() {
+//     console.log("My name is Jonas");
+// }
+
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// const appleOrangeJuce = fruitProcessor(2, 4);
+// console.log(appleOrangeJuce);
+
+//function declaration
+// function calAge1(birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age1 = calAge1(2004);
+// console.log(age1);
+// //function expression
+// const calAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age2 = calAge2(2004);
+
+// console.log(age1, age2);
+
+
+
