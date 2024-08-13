e0c9246711423eaf35894b3fda77983f2473575d
vctduong <165612382+vctduong@users.noreply.github.com>
2024-08-13 09:47:31 +0000

    edit

--- javascript/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ javascript/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -38,24 +38,92 @@
 // console.log(lastName);
 
 
-const now = 2024;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);// 2**3 = 2^3
+// const now = 2024;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);// 2**3 = 2^3
 
-const firstName = 'Jonas';
-const lastName = 'Duong'
-console.log(firstName + " " + lastName);
+// const firstName = 'Jonas';
+// const lastName = 'Duong'
+// console.log(firstName + " " + lastName);
 
 
-let x = 10 + 5;
-x++;
-x--;
+// let x = 10 + 5;
+// x++;
+// x--;
 
-console.log(--x);
-console.log(ageJonas > ageSarah);
+// console.log(--x);
+// console.log(ageJonas > ageSarah);
 
-const isFullAge = ageSarah >= 18;
+// const isFullAge = ageSarah >= 18;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
+
+
+// const firstName = 'Jonas';
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2037;
+
+// const jonas = "I'm " + firstName + ", a " + (year - birthYear) + " year-old " + job;
+// console.log(jonas);
+
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+// console.log(jonasNew);
+
+// console.log(`Just a regular string ....`)
+// console.log('String with \n\
+//     multiple \n\
+//     lines');
+
+// console.log(`String
+// multiple
+// lines`)
+
+
+
+
+
+// const age = 15;
+// if (age >= 18) {
+//     console.log("Sarah can start driving license");
+// } else {
+//     const yearLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearLeft} years :)`);
+// }
+// const birthYear = 1991;
+// let century;
+// if (birthYear <= 2000) {
+//     century = 20;
+
+// } else {
+//     century = 21;
+// }
+// console.log(century)
+
+
+//type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(inputYear + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN); //NaN is an invalid number so typeof NaN is still number
+console.log(String(23), 23);
+//type coercion
+console.log("I am " + 23 + " years old");
+console.log("I am " + '23' + " years old");
+console.log("I am " + String(23) + " years old");
+console.log('23' - '10' - 3);
+console.log('23' + '10' + 3);
+console.log('23' * '2');
+console.log('23' / '2');
+
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n)
+
+let m = '10' - '4' - '3' - 2 + '5'
+console.log(m);
