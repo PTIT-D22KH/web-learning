b326d1c8d889a762c8674d7b0c256b1ca294c3ea
vctduong <165612382+vctduong@users.noreply.github.com>
2024-08-13 08:53:26 +0000

    add files

---
+++ javascript/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,57 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+// console.log("Duong");
+// console.log(23);
+
+// let firstName = "Duong";
+// console.log(firstName);
+
+// let PI = 3.1415;
+
+
+
+
+// let javascriptIsFun = true;
+// console.log(typeof javascriptIsFun);
+// javascriptIsFun = "HELLO";
+// console.log(typeof javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+
+
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// birthYear = 1990;
+
+// const job; // const must be init at declaration
+
+// var job = 'programmer';
+// job = 'teacher';
+// console.log(job);
+
+// lastName = 'Djfasjd';
+// console.log(lastName);
+
+
+const now = 2024;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);// 2**3 = 2^3
+
+const firstName = 'Jonas';
+const lastName = 'Duong'
+console.log(firstName + " " + lastName);
+
+
+let x = 10 + 5;
+x++;
+x--;
+
+console.log(--x);
+console.log(ageJonas > ageSarah);
